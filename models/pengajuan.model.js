ae9a60265cddb0320c1c75d33f4964c3ccc7cebe
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-09-26 18:10:44 +0700

    -

--- models/pengajuan.model.js
+++ models/pengajuan.model.js
@@ -27,7 +27,7 @@ const pengajuanSchema = new mongoose.Schema(
     ttd: { type: String, trim: true },
     status: {
       type: String,
-      enum: ["pending", "approved", "rejected", "draft"],
+      enum: ["pending", "approved", "rejected", "draft", "returned"],
       default: "pending",
     },
     items: {
